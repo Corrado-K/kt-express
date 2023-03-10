59f63c579c7bae2af9451a2bd49f579ea10e64c9
Corrado-K <kebemartey@gmail.com>
2023-03-10 10:07:54 +0000

    Create number square route

---
+++ src/routes/numberSquare.ts
@@ -0,0 +1,27 @@
+
+
+import { Request, Response, Router } from "express";
+import { isNull } from "util";
+
+const router = Router()
+
+router.get('/:number', (req: Request, res: Response) => {
+
+     if(isNaN(Number(req.params.number))){
+          return res.status(400).json({
+               error: "Param not a number"
+          })
+     }
+
+     const numberSquare: number = Math.pow(Number(req.params.number),2)
+     res.json({
+          message: "Success",
+          payload: {
+               number: Number(req.params.number),
+               numberSquare: numberSquare
+          }
+     });
+
+})
+
+export default router
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express"
 import userRouter from './user'
 import blogPostsRouter from './blogPosts'
+import numberSquareRouter from './numberSquare'
 
 const router = Router()
 
 router.use('/api/v1/users', userRouter)
 router.use('/api/v1/blogPosts', blogPostsRouter)
+router.use('/api/v1/numberSquare', numberSquareRouter)
 
 export default router;
